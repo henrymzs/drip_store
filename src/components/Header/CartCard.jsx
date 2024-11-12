49b3eff67a690346f1ee5a8761ed66f7ae249895
Henry <henrykaua21@gmail.com>
2024-11-12 17:06:39 -0300

    Finalizando alterações no header

---
+++ src/components/Header/CartCard.jsx
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from 'react';
+
+export default function CartCard({
+    className = '',
+    image,
+    title,
+    color,
+    size,
+    quantity = 0,
+    showCounter = false,
+    price,
+    total,
+}) {
+    const [count, setCount] = useState(quantity);
+
+    useEffect(() => {
+        setCount(quantity);
+    }, [quantity]);
+
+    const decreaseItem = () => setCount(prevCount => Math.max(prevCount - 1, 0));
+    const increaseItem = () => setCount(prevCount => prevCount + 1);
+    const handleInputChange = (e) => {
+        const newValue = parseInt(e.target.value, 10);
+        setCount(isNaN(newValue) ? 0 : newValue);
+    };
+
+    return (
+        <div className={`${className} lg:flex flex-col`}>
+            {color && size && price && <hr className='my-5 bg-light-gray-2'></hr>}
+            <div className="flex flex-col lg:flex-row justify-between">
+                <div className="flex">
+                    <img
+                        className="h-[104px] w-[127px] mr-5 box-border object-contain bg-light-blue"
+                        src={image}
+                        alt={title}
+                    />
+                    <span>
+                        <h1 className="text-sm font-bold text-dark-gray">{title}</h1>
+                        {!showCounter && price && (
+                            <p className="text-dark-gray-2 text-base font-bold">R$ {price}</p>
+                        )}
+                        {color && (
+                            <p className="py-2 text-sm font-medium text-light-gray">
+                                Cor: <span className="text-dark-gray">{color}</span>
+                            </p>
+                        )}
+                        {size && (
+                            <p className="text-sm font-medium text-light-gray">
+                                Tamanho: <span className="text-dark-gray">{size}</span>
+                            </p>
+                        )}
+                    </span>
+                </div>
+                {showCounter && (
+                    <div className="flex flex-col lg:flex-row lg:items-center gap-7 lg:gap-9">
+                        <div className="flex flex-col items-center lg:pr-2 pt-5 lg:pt-0">
+                            <h1 className="block lg:hidden font-medium font-sm self-start pb-3 text-dark-gray-2">
+                                QUANTIDADE
+                            </h1>
+                            <div className="flex items-center pb-2">
+                                <button
+                                    className="h-9 w-[85px] lg:w-9 border border-light-gray-2 rounded-sm"
+                                    onClick={decreaseItem}
+                                >
+                                    -
+                                </button>
+                                <input
+                                    type="text"
+                                    value={count}
+                                    onChange={handleInputChange}
+                                    className="h-9 w-[85px] lg:w-9 text-center text-dark-gray-2 text-xs"
+                                />
+                                <button
+                                    className="h-9 w-[85px] lg:w-9 border border-light-gray-2 rounded-sm"
+                                    onClick={increaseItem}
+                                >
+                                    +
+                                </button>
+                            </div>
+                            <button
+                                className="text-md hover:font-bold text-red-600 underline"
+                                onClick={decreaseItem}
+                            >
+                                Remover item
+                            </button>
+                        </div>
+                        <span className="flex justify-between w-full">
+                            <h1 className="block lg:hidden font-medium font-sm text-dark-gray-2">UNITÁRIO</h1>
+                            <p className="text-dark-gray-2 text-base font-bold">R$ {price}</p>
+                        </span>
+                        {total && (
+                            <span className="flex justify-between w-full">
+                                <h1 className="block lg:hidden font-medium font-sm text-dark-gray-2">TOTAL</h1>
+                                <p className="text-dark-gray-2 text-base font-bold">R$ {total}</p>
+                            </span>
+                        )}
+                    </div>
+                )}
+            </div>
+            {!showCounter && price && (
+                <div>
+                    <hr className="my-5 bg-light-gray-2" />
+                    <p className="flex justify-between font-bold text-base text-dark-gray">
+                        Valor total: <span className="text-error">R$ {total}</span>
+                    </p>
+                </div>
+            )}
+        </div>
+    );
+}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,122 @@
-import React from 'react';
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { CiSearch } from 'react-icons/ci';
 import Nav from './Nav';
 import Logo from './Logo';
 import Button from './Button';
+import CartCard from './CartCard';
+import carrinho from '../../assets/header/carrinho.svg';
+import nikeG from "../../assets/products/nike-yellow.png";
 
 
+export default function Header() {
+  const [open, setOpen] = useState(false);
+  const [openSearch, setOpenSearch] = useState(false);
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
 
-const Header = () => {
-    return (
-        <header className="py-5 md:flex-col bg-white fixed md:relative w-full z-50 top-0 shadow-md md:shadow-none">
-            <div className="flex justify-between items-center px-5"> 
-            
-                <Logo type="logoHeader" /> {/* Passando a prop type */}
-                <div className="hidden md:flex items-center justify-around bg-neutral-200/80 rounded-md w-1/2">
-                    <form className="flex items-center w-full">
-                        <input
-                            id="search"
-                            type="text"
-                            placeholder="Pesquisar por produtos..."
-                            className="p-2 w-full outline-none bg-transparent focus:border-pink-600 focus:ring-pink-600 focus:ring-2 rounded-md"
-                            onChange={(e) => setQuery(e.target.value)}
-                            />
-                            <button type="submit" className="relative mx-2">
+  const navigate = useNavigate();
 
-                            </button>
-                    </form>
+  const handleOpenMenu = () => {
+    setOpen(!open);
+    setOpenSearch(false);
+  };
+
+  const handleOpenSearch = () => {
+    setOpen(false);
+    setOpenSearch(!openSearch);
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleEntrar = () => navigate("/login");
+  const handleRegister = () => navigate("/register");
+
+  const handleMouseEnter = () => setIsPopupVisible(!isPopupVisible);
+
+  return (
+    <header className="py-5 md:flex-col bg-white fixed md:relative w-full z-50 top-0 shadow-md md:shadow-none">
+      <div className="flex justify-between items-center px-5">
+        <Nav onClick={handleOpenMenu} />
+        <Logo type="logoHeader" />
+
+        <div className="hidden md:flex items-center justify-around bg-neutral-200/80 rounded-md w-1/2">
+          <form onSubmit={handleSearchSubmit} className="flex items-center w-full">
+            <input
+              id="search"
+              type="text"
+              placeholder="Pesquisar por produtos..."
+              className="p-2 w-full outline-none bg-transparent focus:border-pink-600 focus:ring-pink-600 focus:ring-2 rounded-md"
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <button type="submit" className="relative mx-2">
+              <CiSearch size="18px" color="#c8c8c8" className="absolute top-1/2 transform -translate-y-1/2 right-0" />
+            </button>
+          </form>
+        </div>
+
+        <div className="hidden md:flex md:items-center gap-5">
+          <button
+            className="lg:text-lg md:text-sm underline underline-offset-4 hover:text-primary hover:font-medium hover:transition-colors hover:scale-105 ease-out"
+            onClick={handleRegister}
+          >
+            Cadastre-se
+          </button>
+          <Button type="submit" text="Entrar" onClick={handleEntrar} />
+        </div>
+
+        <div className="flex items-center justify-around gap-1">
+          <CiSearch
+            color="#c8c8c8"
+            className="h-auto w-6 md:hidden"
+            onClick={handleOpenSearch}
+          />
+          <div className="flex flex-col md:hidden">
+            {openSearch && (
+              <div className="flex w-screen absolute p-2 top-16 left-0 bg-white shadow-lg z-50">
+                <form onSubmit={handleSearchSubmit} className="flex w-full">
+                  <input
+                    id="pesquisar"
+                    type="text"
+                    placeholder="Pesquisar por produto..."
+                    className="p-2 w-full rounded-md bg-neutral-200 outline-none focus:border-pink-600 focus:ring-pink-600 focus:ring-2"
+                    onChange={(e) => setQuery(e.target.value)}
+                  />
+                  <button type="submit">
+                    <CiSearch color="#c8c8c8" className="h-auto w-8" />
+                  </button>
+                </form>
+              </div>
+            )}
+          </div>
+
+          <div className="relative" onClick={handleMouseEnter}>
+            <img src={carrinho} alt="Icon Cart" className="h-auto w-6 cursor-pointer" />
+            {isPopupVisible && (
+              <div className="flex flex-col absolute right-1 top-full w-[315px] mt-1 px-5 py-8 z-50 bg-white shadow-lg border border-gray-300">
+                <h1 className="font-bold text-base text-dark-gray-2">Meu Carrinho</h1>
+                <hr className='my-5 bg-light-gray-2'></hr>
+                <CartCard 
+                    className="flex flex-row"
+                    image={nikeG} 
+                    title="K-swiss V8 - Masculino" 
+                    quantity={2}
+                    price={219.00}
+                    total={219.00}
+                />
+                <div className="flex justify-between items-center pt-5">
+                  <NavLink to="/cart" className='text-sm font-medium text-dark-gray-2 underline'>Esvaziar</NavLink>
+                  <NavLink to="/cart" className="p-2 rounded-xl bg-primary text-white hover:scale-105 hover:cursor-pointer hover:bg-pink-500 transition-colors">Ver Carrinho</NavLink>
                 </div>
-                <Nav />
-                <Button />
-            </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
 
-        </header>
-    );
-};
-
-export default Header;
+      
+      
+    </header>
+  );
+}
