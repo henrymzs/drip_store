b96a08157fac224deee949d7e19fb6c671e98e75
Henry <henrykaua21@gmail.com>
2024-11-12 22:51:26 -0300

    Revert "atualização footer"
    This reverts commit 46037f562baf04ef65d3371bfdca49c7f3cc6f60, reversing
    changes made to 49b3eff67a690346f1ee5a8761ed66f7ae249895.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
 import React from "react";
 import Header from "./components/Header/Header";
-import Footer from "../src/components/Footer/Footer";
-import {Routes, Route } from 'react-router-dom';
-import ProductViewPage from "./pages/ProductViewPage";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import HomePage from "./pages/HomePage";
 
-function App() {
+const App = () => {
   return (
     <>
-    
-      {/* Header fixo no topo */}
-      <Header />
-      {/* Área principal onde as rotas são renderizadas */}
-      <main>
-        <Routes>
-           {/* Rota para a página inicial */}
-           <Route path="/" element={<HomePage />} />
-          {/* Rota para a página de visualização do produto */}
-          <Route path="/product/:id" element={<ProductViewPage />} />
-        </Routes>
-      </main>
-
-      {/* Footer fixo no final */}
-      <Footer />
-
-     </>
-    
+    <Header />
+    </>
   );
-}
+};
 
 export default App;
--- src/pages/HomePage.jsx
+++
@@ -1,5 +0,0 @@
-function HomePage() {
-    return <div>Bem-vindo à página inicial!</div>;
-  }
-  
-  export default HomePage;
