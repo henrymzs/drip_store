2f262da7f81fc80fd5e13f7801190421081391e2
Nathan-Mn <antnat48@gmail.com>
2024-11-15 00:20:03 -0300

    Componentes do Nathan

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,18 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import SquareCollection from "./components/SquareCollection/SquareCollection";
 import CircleCollection from './components/CircleCollection/CircleCollection';
+import SpecialOffer from "./components/SpecialOffer/Special_Offer";
+import Section from "./components/Section/Section";
+import ProductListing from "./components/ProductListing/ProductListing";
+import ProductCard from "./components/ProductCard/ProductCard";
 const App = () => {
   return (
     <>
     <Header />
     <SquareCollection />
+    <SpecialOffer/>
     <CircleCollection />
+    <ProductListing/>
     <Footer />
     </>
   );
---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import imageCard from '../../assets/collections/tenis.svg'
+import './ProductCard.css'
+
+const baseProducts = {
+    name: "K-Swiss V8 - Masculino",
+    image: imageCard,
+    subName: "Tênis",
+    price: "$200",
+    priceDiscount: "$100"
+};
+
+export default function ProductCard({ quantidade }) {
+    const products = Array(quantidade).fill(baseProducts);
+
+    return (
+        <>
+            {products.map((product, index) => (
+                <a href='/ProductViewPage' className='productCardA'>
+                    <div key={index} className="cardPrincipal">
+                        <div className="cardImagem">
+                            <img src={product.image} alt="Imagem do ProductCard" />
+                        </div>
+                        <p className='tinny'>{product.subName}</p>
+                        <p className='large'>{product.name}</p>
+                        <div className='cardValor'>
+                            <p className="large2">{product.price}</p>
+                            <p className="largeBold">{product.priceDiscount}</p>
+                        </div>
+                    </div>
+                </a>
+
+            ))}
+        </>
+    );
+}
